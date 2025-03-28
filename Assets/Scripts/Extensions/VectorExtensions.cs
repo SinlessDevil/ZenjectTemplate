5ca20b4df281d466eac1aa13e7c1eef749d181a2
SinlessDevil <83553523+SinlessDevil@users.noreply.github.com>
2025-03-28 11:00:50 +0200

    Added extensions

--- Assets/Scripts/Extensions/VectorExtensions.cs
+++ Assets/Scripts/Extensions/VectorExtensions.cs
@@ -1,43 +1,79 @@
 ﻿using UnityEngine;
 
-namespace Extensions
+namespace Code.Common.Extensions
 {
     public static class VectorExtensions
     {
-        public static Vector3 AddX(this Vector3 vector, float value)
+        public static Vector3 SetX(this Vector3 v, float x)
         {
-            vector.x += value;
-            return vector;
+            var tmp = v;
+            tmp.x = x;
+            v = tmp;
+            return v;
         }
-        
-        public static Vector3 AddY(this Vector3 vector, float value)
+
+        public static Vector3 SetY(this Vector3 v, float y)
         {
-            vector.y += value;
-            return vector;
+            var tmp = v;
+            tmp.y = y;
+            v = tmp;
+            return v;
         }
-        
-        public static Vector3 AddZ(this Vector3 vector, float value)
+
+        public static Vector3 SetZ(this Vector3 v, float z)
         {
-            vector.z += value;
-            return vector;
+            var tmp = v;
+            tmp.z = z;
+            v = tmp;
+            return v;
         }
-        
-        public static Vector3 SetX(this Vector3 vector, float value)
+
+        public static Vector2 SetX(this Vector2 v, float x)
         {
-            vector.x = value;
-            return vector;
+            var tmp = v;
+            tmp.x = x;
+            v = tmp;
+            return v;
         }
-        
-        public static Vector3 SetY(this Vector3 vector, float value)
+
+        public static Vector2 SetY(this Vector2 v, float y)
         {
-            vector.y = value;
-            return vector;
+            var tmp = v;
+            tmp.y = y;
+            v = tmp;
+            return v;
         }
-        
-        public static Vector3 SetZ(this Vector3 vector, float value)
+
+        public static Vector3 AddX(this Vector3 v, float xDelta)
         {
-            vector.z = value;
-            return vector;
+            var tmp = v;
+            tmp.x = tmp.x + xDelta;
+            v = tmp;
+            return v;
+        }
+
+        public static Vector3 AddY(this Vector3 v, float yDelta)
+        {
+            var tmp = v;
+            tmp.y = tmp.y + yDelta;
+            v = tmp;
+            return v;
+        }
+
+        public static Vector2 AddX(this Vector2 v, float xDelta)
+        {
+            var tmp = v;
+            tmp.x = tmp.x + xDelta;
+            v = tmp;
+            return v;
+        }
+
+        public static Vector2 AddY(this Vector2 v, float yDelta)
+        {
+            var tmp = v;
+            tmp.y = tmp.y + yDelta;
+            v = tmp;
+            return v;
         }
     }
 }
